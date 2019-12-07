ac381924b2e1cc9f600d837074f4270a79055e4c
shenzaifang <shenzf@yonyou.com>
2019-12-07 14:44:33 +0800

    修改单元测试

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,8 +4,9 @@ const assert = require('assert');
 describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js')
 
+    console.log(baseConfig)
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, '/Users/cpselvis/my-project/builder-webpack/test/smoke/template/src/index/index.js');
-        assert.equal(baseConfig.entry.search, '/Users/cpselvis/my-project/builder-webpack/test/smoke/template/src/search/index.js');
+        assert.equal(baseConfig.entry.index, '/Users/shenzaifang/shenzf/builder-webpack/test/smoke/template/src/index/index.js');
+        assert.equal(baseConfig.entry.search, '/Users/shenzaifang/shenzf/builder-webpack/test/smoke/template/src/search/index.js');
     });
 });
