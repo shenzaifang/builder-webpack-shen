424c62467b05a5637d6f034fc8630fb4cae256d0
shenzaifang <shenzf@yonyou.com>
2019-12-16 11:23:08 +0800

    chore： 构建时分离 react/react-dom

---
+++ src/index/index.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * Created by shenzaifang on 2019-12-16
+ */
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+    `<div>reactDom<div>`,
+    document.getElementById('root')
+);
+
